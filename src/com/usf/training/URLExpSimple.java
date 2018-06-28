17aba46e01aa1df4da54a16067cbd76afd011b25
Shushuma Kotturu <skotturu@usf.edu>
2018-06-28 11:09:54 -0400

    Vesrion 2 - Added connection success implementaion

--- src/com/usf/training/URLExpSimple.java
+++ src/com/usf/training/URLExpSimple.java
@@ -1,9 +1,33 @@
 package com.usf.training;
 
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
 public class URLExpSimple {
 
 	public boolean isWebConnectionSuccessful(String url) {
 		boolean canConnecttoURL = false;
+        try {
+            URL mySite = new URL(url);
+            URLConnection yc = mySite.openConnection();
+            Scanner in = new Scanner(new InputStreamReader(yc.getInputStream()));
+            int count = 0;
+            while (in.hasNext()) {
+                System.out.println(in.next());
+                count++;
+                break;
+            }
+            //System.out.println("Number of tokens: " + count);
+   
+            in.close();
+            if(count > 0) {
+            	canConnecttoURL = true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 		return canConnecttoURL;
 	}
     
